1aca67c2df16a529a59873d1224ac78e764e4630
ID <115931028+AidaJalali@users.noreply.github.com>
2023-05-20 19:52:27 +0330

    add database and user class

--- AA/src/main/java/controller/RegisterAndLoginMenuController.java
+++ AA/src/main/java/controller/RegisterAndLoginMenuController.java
@@ -1,21 +1,50 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterAndLoginMenuController {
     private PasswordField password;
     private TextField username;
-
     public void register(ActionEvent event){
+        String usernameError = checkUsernameError();
+        String passwordError = checkPasswordError();
+        if(usernameError == null && passwordError == null){
+            //CreateAccount();
+            //TODO this part is related to database
+        }
+        else if(usernameError != null){
 
+        }
+        else if (passwordError != null) {
+
+        }
     }
 
     public void login(ActionEvent event){
-
+        //TODO this part is related to database
     }
     public void reset(ActionEvent event){
-
+        password.setText("");
+        username.setText("");
     }
+    public String checkUsernameError(){
+     if(!username.getText().matches("\\S+"))
+         return "Username is invalid!";
+     if(username.getText().length() < 5)
+         return "Username is too short!";
+     if(username.getText().length() > 10)
+         return "Username is too long!";
+     return null;
+    }
+    public String checkPasswordError(){
+        if(!username.getText().matches("\\S+"))
+            return "Username is invalid!";
+        if(username.getText().length() < 5)
+            return "Password is week!";
+        return null;
+    }
+
 }
--- AA/src/main/java/module-info.java
+++ AA/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module AA {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
     exports controller;
     opens controller to javafx.fxml;
     exports model;
+    opens model to com.google.gson;
     exports view.menu;
     opens view.menu to javafx.fxml;
+
 }
 
